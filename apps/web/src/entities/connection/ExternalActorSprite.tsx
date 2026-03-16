42bf784ef4cd67c909c272bf22452b03d73a54ee
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-03-16 09:08:22 +0900

    Refactor ExternalActorSprite to use pre-made SVG image
    - Replace inline SVG cloud and styled divs with internet.svg sprite
    - Simplify CSS from complex multi-element layout to single image
    - Consistent rendering approach with BlockSprite and PlateSprite

--- apps/web/src/entities/connection/ExternalActorSprite.tsx
+++ apps/web/src/entities/connection/ExternalActorSprite.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 import type { ExternalActor } from '../../shared/types/index';
+import internetSprite from '../../shared/assets/actor-sprites/internet.svg';
 import './ExternalActorSprite.css';
 
 interface ExternalActorSpriteProps {
@@ -10,7 +11,6 @@ interface ExternalActorSpriteProps {
 }
 
 export const ExternalActorSprite = memo(function ExternalActorSprite({
-  actor,
   screenX,
   screenY,
   zIndex,
@@ -24,19 +24,12 @@ export const ExternalActorSprite = memo(function ExternalActorSprite({
         zIndex,
       }}
     >
-      <div className="actor-icon">
-        <svg viewBox="0 0 64 40" width="64" height="40" role="img" aria-label="Internet Cloud">
-          <title>Internet Cloud</title>
-          <path d="M52 28H14c-5.5 0-10-4.5-10-10 0-4.8 3.4-8.8 8-9.7C13.2 3.7 17.8 0 23.2 0c4.5 0 8.5 2.5 10.5 6.2C35 5.5 36.8 5 38.8 5c5 0 9.2 3.5 10.2 8.2C53.5 14.2 57 18.2 57 23c0 2.8-2.2 5-5 5z" fill="#2196F3"/>
-        </svg>
-      </div>
-      <div className="actor-block">
-        <div className="actor-studs">
-          <div className="stud"></div>
-          <div className="stud"></div>
-        </div>
-        <div className="actor-label">{actor.name}</div>
-      </div>
+      <img
+        className="actor-img"
+        src={internetSprite}
+        alt="Internet"
+        draggable={false}
+      />
     </div>
   );
 });
