5f1782a9642abcbcd287f900602620bd199edefe
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-03-18 08:45:59 +0900

    refactor(generate): restructure providers into per-provider directories with AWS/GCP stubs (#90)
    * refactor(generate): restructure providers into per-provider directories with AWS/GCP stubs
    
    Closes #82
    
    * fix(generate): update unknown provider test for multi-provider registry

---
+++ apps/web/src/features/generate/providers/gcp/index.ts
@@ -0,0 +1,87 @@
+import type { ProviderAdapter, ProviderDefinition } from '../../types';
+
+export const gcpProviderDefinition: ProviderDefinition = {
+  name: 'gcp',
+  displayName: 'GCP',
+  blockMappings: {
+    compute: {
+      resourceType: 'google_compute_instance',
+      namePrefix: 'instance',
+    },
+    database: {
+      resourceType: 'google_sql_database_instance',
+      namePrefix: 'sql',
+    },
+    storage: {
+      resourceType: 'google_storage_bucket',
+      namePrefix: 'bucket',
+    },
+    gateway: {
+      resourceType: 'google_compute_url_map',
+      namePrefix: 'urlmap',
+    },
+    function: {
+      resourceType: 'google_cloudfunctions_function',
+      namePrefix: 'function',
+    },
+    queue: {
+      resourceType: 'google_pubsub_topic',
+      namePrefix: 'topic',
+    },
+    event: {
+      resourceType: 'google_eventarc_trigger',
+      namePrefix: 'trigger',
+    },
+    timer: {
+      resourceType: 'google_cloud_scheduler_job',
+      namePrefix: 'schedule',
+    },
+  },
+  plateMappings: {
+    network: {
+      resourceType: 'google_compute_network',
+      namePrefix: 'network',
+    },
+    subnet: {
+      resourceType: 'google_compute_subnetwork',
+      namePrefix: 'subnet',
+    },
+  },
+  generators: {
+    terraform: {
+      requiredProviders: () =>
+        [
+          'terraform {',
+          '  required_providers {',
+          '    google = {',
+          '      source  = "hashicorp/google"',
+          '      version = "~> 5.0"',
+          '    }',
+          '  }',
+          '}',
+        ].join('\n'),
+      providerBlock: (region: string) =>
+        [
+          'provider "google" {',
+          `  region = "${region}"`,
+          '}',
+        ].join('\n'),
+    },
+    bicep: {
+      targetScope: 'resourceGroup',
+    },
+    pulumi: {
+      packageName: '@pulumi/gcp',
+      runtime: 'nodejs',
+    },
+  },
+};
+
+export const gcpProvider: ProviderAdapter = {
+  name: gcpProviderDefinition.name,
+  displayName: gcpProviderDefinition.displayName,
+  blockMappings: gcpProviderDefinition.blockMappings,
+  plateMappings: gcpProviderDefinition.plateMappings,
+  providerBlock: gcpProviderDefinition.generators.terraform.providerBlock,
+  requiredProviders: gcpProviderDefinition.generators.terraform.requiredProviders,
+};
